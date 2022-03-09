ba817867a0a463cac022515ee53e54bcaa9c4a42
tmfrnz <timo@dumpark.com>
2022-03-09 23:00:18 +0100

    connections are stored in groups

--- app/utils/fields.js
+++ app/utils/fields.js
@@ -323,6 +323,7 @@ export const getActorConnectionField = ({
       entityType: 'actions',
       path: API.ACTIONS,
       clientPath: ROUTES.ACTION,
+      groupByType: true,
     },
     targets: {
       message: 'entities.actions_{typeid}.plural',
@@ -330,6 +331,7 @@ export const getActorConnectionField = ({
       entityTypeAs: 'targetingActions',
       path: API.ACTIONS,
       clientPath: ROUTES.ACTION,
+      groupByType: true,
     },
     members: {
       message: 'entities.actors_{typeid}.plural',
@@ -337,6 +339,7 @@ export const getActorConnectionField = ({
       entityTypeAs: 'members',
       path: API.ACTORS,
       clientPath: ROUTES.ACTOR,
+      groupByType: true,
     },
     associations: {
       message: 'entities.actors_{typeid}.plural',
@@ -344,6 +347,7 @@ export const getActorConnectionField = ({
       entityTypeAs: 'associations',
       path: API.ACTORS,
       clientPath: ROUTES.ACTOR,
+      groupByType: true,
     },
   },
   entityType: typeid ? `actors_${typeid}` : 'actors',
@@ -371,6 +375,7 @@ export const getActionConnectionField = ({
       entityType: 'actors',
       path: API.ACTORS,
       clientPath: ROUTES.ACTOR,
+      groupByType: true,
     },
     targets: {
       message: 'entities.actors_{typeid}.plural',
@@ -378,12 +383,14 @@ export const getActionConnectionField = ({
       entityTypeAs: 'targets',
       path: API.ACTORS,
       clientPath: ROUTES.ACTOR,
+      groupByType: true,
     },
     resources: {
       message: 'entities.resources_{typeid}.plural',
       entityType: 'resources',
       path: API.RESOURCES,
       clientPath: ROUTES.RESOURCE,
+      groupByType: true,
     },
   },
   entityType: typeid ? `actions_${typeid}` : 'actions',
@@ -408,6 +415,7 @@ export const getResourceConnectionField = ({
       entityType: 'actions',
       path: API.ACTIONS,
       clientPath: ROUTES.ACTION,
+      groupByType: true,
     },
   },
   entityType: typeid ? `resources_${typeid}` : 'resources',
