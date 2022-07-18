ddca33d40d86e636a9000810aed127c0f381ef60
tmfrnz <timo@dumpark.com>
2022-07-18 13:33:40 +0200

    increment version

--- app/themes/config.js
+++ app/themes/config.js
@@ -14,7 +14,7 @@ export const NODE_ENV = sessionStorage.NODE_ENV || 'production';
 
 const IS_DEV = true;
 
-export const version = '1.0.7[dev:pilot-projects]';
+export const version = '1.1.0[dev:pilot-projects]';
 
 export const ENDPOINTS = {
   API: (
