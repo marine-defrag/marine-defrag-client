ece51a0e9e0f5c95fe93e88896e90a3eb5cb8a89
vbojilova <viki.bojilova@gmail.com>
2023-07-21 17:30:03 +1200

    fixed highlight category title - #113

--- app/containers/EntityList/EntitiesCategories.js
+++ app/containers/EntityList/EntitiesCategories.js
@@ -5,17 +5,20 @@ import { Box } from 'grommet';
 
 import qe from 'utils/quasi-equals';
 import appMessages from 'containers/App/messages';
-import { injectIntl, intlShape } from 'react-intl';
-
+import { injectIntl, intlShape, FormattedMessage } from 'react-intl';
 import ButtonDefault from 'components/buttons/ButtonDefault';
+import messages from './messages';
 
 const ButtonWrapper = styled((p) => (
   <Box direction="row" justify="start" wrap gap="small" {...p} />
 ))`
   overflow-x: auto;
 `;
+const HighlightCategoryLabel = styled.span`
+font-size:12px;
+color: #a9abad;
+`;
 const Styled = styled.div``;
-const StyledText = styled.span``;
 const TaxonomyGroup = styled.div``;
 
 const EntitiesCategories = ({
@@ -28,7 +31,14 @@ const EntitiesCategories = ({
   <Styled>
     {taxonomiesWithCats.map((taxonomy) => (
       <TaxonomyGroup key={taxonomy.id}>
-        <StyledText>{intl.formatMessage(appMessages.entities.taxonomies[taxonomy.id].plural)}</StyledText>
+        <HighlightCategoryLabel>
+          <FormattedMessage
+            {...messages.highlightCategory}
+            values={{
+              categoryName: intl.formatMessage(appMessages.entities.taxonomies[taxonomy.id].plural),
+            }}
+          />
+        </HighlightCategoryLabel>
         <ButtonWrapper>
           {taxonomy.categories.map((category) => {
             const { id, label } = category;
