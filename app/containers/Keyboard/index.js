1ced7a6b1766a1b4020af75196a6248f63d2125c
vbojilova <viki.bojilova@gmail.com>
2024-05-27 13:11:03 -0700

    added more keyboard functions and inner span styling to keyboard listener #141

--- app/containers/Keyboard/index.js
+++ app/containers/Keyboard/index.js
@@ -5,12 +5,11 @@ import { Keyboard as KeyboardListener } from 'grommet';
 import styled from 'styled-components';
 
 const StyledSpan = styled.span`
-  width: inherit;
 `;
 
-const Keyboard = ({ children, ...props }) => (
+const Keyboard = ({ children, spanStyle, ...props }) => (
   <KeyboardListener {...props}>
-    <StyledSpan>
+    <StyledSpan style={spanStyle}>
       {children}
     </StyledSpan>
   </KeyboardListener>
@@ -18,10 +17,19 @@ const Keyboard = ({ children, ...props }) => (
 
 Keyboard.propTypes = {
   children: PropTypes.node,
+  onBackspace: PropTypes.func,
+  onComma: PropTypes.func,
+  onDown: PropTypes.func,
   onTab: PropTypes.func,
   onEnter: PropTypes.func,
   onEsc: PropTypes.func,
   onKeyDown: PropTypes.func,
+  onLeft: PropTypes.func,
+  onRight: PropTypes.func,
+  onShift: PropTypes.func,
+  onSpace: PropTypes.func,
+  onUp: PropTypes.func,
+  spanStyle: PropTypes.object,
 };
 
 export default Keyboard;
